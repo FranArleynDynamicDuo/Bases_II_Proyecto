964b208d86f8ad9c69826127325a5f8c04f29a5d
Francisco Javier Sucre González <frank91frank@gmail.com>
2016-08-18 19:53:15 -0400

    (FJSG) Insert y delete funcionan aqui

--- Entrega_II/ScriptFinal.sql
+++ Entrega_II/ScriptFinal.sql
@@ -64,17 +64,6 @@ CREATE OR REPLACE TRIGGER barco_update_not_null
     END;
 /
 
-CREATE OR REPLACE TRIGGER barco_update_null
-    AFTER UPDATE OF realiza_ruta ON Barco FOR EACH ROW
-    WHEN (NEW.realiza_ruta IS NULL)
-    BEGIN
-        /* Libero la ruta antigua  */
-        UPDATE Ruta r 
-        SET r.es_realizada = NULL
-        WHERE REF(r) = :OLD.realiza_ruta;
-    END;
-/
-
 /* DELETE */
 /* En caso de que borremos un barco debemos cambiar la referencia inversa a la ruta que tenia asignada */
 CREATE OR REPLACE TRIGGER barco_delete
@@ -123,16 +112,6 @@ CREATE OR REPLACE TRIGGER ruta_update_after
     END;
 /
 
-CREATE OR REPLACE TRIGGER ruta_update_after_null
-    AFTER UPDATE ON Ruta FOR EACH ROW
-    WHEN (NEW.es_realizada IS NULL)
-    BEGIN
-        /* Libero la ruta antigua  */
-        UPDATE Barco b 
-        SET b.realiza_ruta = NULL
-        WHERE REF(b) = :OLD.es_realizada;
-    END;
-/
 
 /* DELETE */
 /* En caso de que borremos una ruta debemos cambiar la referencia inversa al barco al que estaba asignada */
@@ -149,9 +128,10 @@ CREATE OR REPLACE TRIGGER ruta_delete
 ALTER TABLE Barco ENABLE ALL TRIGGERS;
 ALTER TABLE Ruta ENABLE ALL TRIGGERS;
 
-/* -------------------------------------------- INSERCIONES -------------------------------------------- */
+/* -------------------------------------------- PRUEBAS INSERT -------------------------------------------- */
 
-/* INSTANCIAS PARA BARCOS */
+
+    /* PODEMOS CREAR BARCOS SIN RUTAS ASIGNADAS */
     INSERT ALL 
         INTO Barco (id,nombre, capacidad, nro_cabinas, nro_cubiertas, tonelaje, eslora,realiza_ruta) VALUES
                     (1,'Buque ZENITH-ZT', 1442, 720, 10, 46811, 208,NULL)
@@ -160,19 +140,34 @@ ALTER TABLE Ruta ENABLE ALL TRIGGERS;
         INTO Barco (id,nombre, capacidad, nro_cabinas, nro_cubiertas, tonelaje, eslora,realiza_ruta) VALUES
                     (3,'Buque SOVEREIGN', 2733, 1162, 12, 73592, 268,NULL)
     SELECT * from dual;
-
-/* INSTANCIAS PARA RUTAS */
+    /* AL CREAR UNA RUTA CON BARCO ASIGNADO, SE ACTUALIZAN LAS REFERENCIAS EN LOS BARCOS CORRESPONDIENTES */
     INSERT ALL
         INTO Ruta (id,nombre, regimen, es_realizada) VALUES 
                 (1,'2 Noches - Andalucia', 'Todo Incluido',
-                ( SELECT REF(oc) FROM Barco oc WHERE oc.nombre = 'Buque SOVEREIGN' ) )
+                ( SELECT REF(oc) FROM Barco oc WHERE oc.id = 1 ) )
         INTO Ruta (id,nombre, regimen, es_realizada) VALUES 
                 (2,'2 Noches - Barcelona', 'Todo Incluido',
-                ( SELECT REF(oc) FROM Barco oc WHERE oc.nombre = 'Buque ZENITH-ZT' ) )
+                ( SELECT REF(oc) FROM Barco oc WHERE oc.id = 2 ) )
         INTO Ruta (id,nombre, regimen, es_realizada) VALUES 
                 (3,'4 Noches - Caribe Sur  (Puerto Limon)', 'Todo Incluido',
-                ( SELECT REF(oc) FROM Barco oc WHERE oc.nombre = 'Buque MONARCH') )
+                ( SELECT REF(oc) FROM Barco oc WHERE oc.id = 3) )
     SELECT * from dual;
+    /* PODEMOS CREAR RUTAS SIN BARCO ASIGNADO */
+    INSERT INTO Ruta (id,nombre, regimen, es_realizada) VALUES 
+        (4,'2 Noches - Venezuela', 'Todo Incluido',NULL );
+    /* AL CREAR BARCOS CON RUTAS ASIGNADAS LAS REFERENCIAS DE LAS RUTAS A LOS BARCOS SE ACTUALIZA */
+    INSERT INTO Barco (id,nombre, capacidad, nro_cabinas, nro_cubiertas, tonelaje, eslora,realiza_ruta) VALUES
+                        (4,'Buque GOVERNANT', 2733, 1162, 12, 73592, 268,
+                        (SELECT REF(oc) FROM Ruta oc WHERE oc.id = 4 ));
 
+/* -------------------------------------------- PRUEBAS UPDATE -------------------------------------------- */
+
+/* -------------------------------------------- PRUEBAS DELETE -------------------------------------------- */
+
+    DELETE FROM Barco
+    WHERE id=4;
+
+    DELETE FROM Ruta
+    WHERE id=1;
 
 /* -------------------------------------------- CONSULTAS -------------------------------------------- */
