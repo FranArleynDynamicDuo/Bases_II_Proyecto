e183394886afd6d810adb9eba21911ba31be1b97
Francisco Javier Sucre González <frank91frank@gmail.com>
2016-08-18 15:36:19 -0400

    (FJSG) Delete estable no se que hacer con el update

--- Entrega_II/ScriptFinal.sql
+++ Entrega_II/ScriptFinal.sql
@@ -8,6 +8,7 @@
 /* -------------------------------------------- CREACION -------------------------------------------- */
 
     CREATE TYPE barco_t AS Object (
+        id INTEGER,
         nombre VARCHAR(50),
         capacidad INTEGER,
         nro_cabinas INTEGER,
@@ -18,6 +19,7 @@
     /
     
     CREATE TYPE ruta_t AS Object (
+        id INTEGER,
         nombre    VARCHAR(100),
         regimen        VARCHAR(50),
         es_realizada   REF barco_t
@@ -26,8 +28,8 @@
 
     ALTER TYPE barco_t add attribute (realiza_ruta REF ruta_t) CASCADE;
 
-    CREATE TABLE Barco OF barco_t ( nombre NOT NULL, PRIMARY KEY (nombre) ) OBJECT IDENTIFIER IS SYSTEM GENERATED;
-    CREATE TABLE Ruta OF ruta_t ( nombre NOT NULL, PRIMARY KEY (nombre) ) OBJECT IDENTIFIER IS SYSTEM GENERATED;
+    CREATE TABLE Barco OF barco_t ( id NOT NULL, PRIMARY KEY (id) ) OBJECT IDENTIFIER IS SYSTEM GENERATED;
+    CREATE TABLE Ruta OF ruta_t ( id NOT NULL, PRIMARY KEY (id) ) OBJECT IDENTIFIER IS SYSTEM GENERATED;
 
 /* -------------------------------------------- TRIGGERS -------------------------------------------- */
 
@@ -35,23 +37,7 @@
 /* INSERT */
 /* En caso de que insertemos una nueva ruta con barco asignado, debemos 
 asignarle la ruta al nuevo barco */
-CREATE OR REPLACE TRIGGER barco_insert_after
-    AFTER INSERT ON Barco FOR EACH ROW
-    WHEN (NEW.realiza_ruta IS NOT NULL)
-    DECLARE
-        PRAGMA AUTONOMOUS_TRANSACTION;
-    BEGIN
-        /* Asignamos la nueva ruta al barco */
-        UPDATE Ruta r 
-        SET r.es_realizada = NULL
-        WHERE REF(r) = :NEW.realiza_ruta;
-        /* Asignamos la nueva ruta al barco */
-        UPDATE Ruta r 
-        SET r.es_realizada = (SELECT REF(b) FROM Barco b WHERE b.nombre = :NEW.nombre)
-        WHERE REF(r) = :NEW.realiza_ruta;
-        COMMIT;
-    END;
-/
+
 /* UPDATE */
 
 /* DELETE */
@@ -70,21 +56,43 @@ CREATE OR REPLACE TRIGGER barco_delete
 /* INSERT */
 /* En caso de que insertemos una nueva ruta con barco asignado, debemos 
 asignarle la ruta al barco */
+
 CREATE OR REPLACE TRIGGER ruta_insert_after
     AFTER INSERT ON Ruta FOR EACH ROW
     WHEN (NEW.es_realizada IS NOT NULL)
     DECLARE
         PRAGMA AUTONOMOUS_TRANSACTION;
+        reference_Count int;
+        referencia_Unica  EXCEPTION;  -- declare exception
     BEGIN
-        /* Le quitamos la ruta al barco deseado para que su propio trigger se encargue de dejar la ruta antigua en null */
+        /* Si el barco deseado tenia una ruta asignada, libero esa ruta  */
+        UPDATE Ruta r 
+        SET r.es_realizada=NULL
+        WHERE EXISTS (SELECT b.id FROM Barco b WHERE REF(b) = :NEW.es_realizada AND REF(r)=b.realiza_ruta);
+        /* Libero el barco de su ruta actual */
         UPDATE Barco b 
-        SET b.realiza_ruta = NULL 
+        SET b.realiza_ruta=NULL
         WHERE REF(b) = :NEW.es_realizada;
-        /* Asignamos la nueva ruta al barco */
+        /* Actualizo el barco con su nueva ruta */
         UPDATE Barco b 
-        SET b.realiza_ruta = (SELECT REF(r) FROM Ruta r WHERE r.nombre = :NEW.nombre)
+        SET b.realiza_ruta= (SELECT REF(r) FROM Ruta r WHERE r.id = :NEW.id)
         WHERE REF(b) = :NEW.es_realizada;
+
+        SELECT COUNT(r.id) INTO reference_Count
+        FROM Ruta r WHERE r.id = :NEW.id;
+        
+        DBMS_OUTPUT.PUT_LINE ('reference_Count = ' || reference_Count);
+        DBMS_OUTPUT.PUT_LINE ('NEW.id = ' || :NEW.id);
+
+        IF (reference_Count = 0) THEN
+            RAISE referencia_Unica;
+        END IF;
+
         COMMIT;
+
+    EXCEPTION
+        WHEN referencia_Unica THEN  -- handle exception
+            DBMS_OUTPUT.PUT_LINE ('NO ENCONTRO NADA');       
     END;
 /
 
@@ -92,23 +100,42 @@ CREATE OR REPLACE TRIGGER ruta_insert_after
 
 /* En Caso de que cambiemos el barco de una ruta debemos dejar a la ruta que estaba asignada anteriormente
 sin barco y actualizar la ruta del barco asignado */
-CREATE OR REPLACE TRIGGER ruta_update_not_null
+CREATE OR REPLACE TRIGGER ruta_update_after
     AFTER UPDATE ON Ruta FOR EACH ROW
-    WHEN (NEW.es_realizada IS NOT NULL AND NEW.es_realizada != OLD.es_realizada)
+    WHEN (NEW.es_realizada IS NOT NULL AND (NEW.es_realizada != OLD.es_realizada OR OLD.es_realizada IS NULL))
     DECLARE
         PRAGMA AUTONOMOUS_TRANSACTION;
+        reference_Count int;
+        referencia_Unica  EXCEPTION;  -- declare exception
     BEGIN
-        /* Le quitamos la ruta al barco deseado para que su propio trigger se encargue de dejar la ruta antigua en null */
+        /* Si el barco deseado tenia una ruta asignada, libero esa ruta  */
+        UPDATE Ruta r 
+        SET r.es_realizada=NULL
+        WHERE EXISTS (SELECT b.id FROM Barco b WHERE REF(b) = :NEW.es_realizada AND REF(r)=b.realiza_ruta);
+        /* Actualizo el barco con su nueva ruta */
         UPDATE Barco b 
-        SET b.realiza_ruta = NULL 
+        SET b.realiza_ruta= (SELECT REF(r) FROM Ruta r WHERE r.id = :NEW.id)
         WHERE REF(b) = :NEW.es_realizada;
-        /* Actualizamos al barco con su nueva ruta */
-        UPDATE Barco b
-        SET b.realiza_ruta = (SELECT REF(r) FROM Ruta r WHERE r.nombre = :NEW.nombre) 
-        WHERE REF(b) = :NEW.es_realizada;
+        /* */
+        SELECT COUNT(r.id) INTO reference_Count
+        FROM Ruta r WHERE r.id = :NEW.id;
+        
+        DBMS_OUTPUT.PUT_LINE ('reference_Count = ' || reference_Count);
+        DBMS_OUTPUT.PUT_LINE ('NEW.id = ' || :NEW.id);
+        DBMS_OUTPUT.PUT_LINE ('OLD.id = ' || :OLD.id);
+
+        IF (reference_Count = 0) THEN
+            RAISE referencia_Unica;
+        END IF;
+
         COMMIT;
+
+    EXCEPTION
+        WHEN referencia_Unica THEN  -- handle exception
+            DBMS_OUTPUT.PUT_LINE ('ERROR: NO ENCONTRO NADA');       
     END;
 /
+
 /* DELETE */
 /* En caso de que borremos una ruta debemos cambiar la referencia inversa al barco al que estaba asignada */
 CREATE OR REPLACE TRIGGER ruta_delete
@@ -128,38 +155,26 @@ ALTER TABLE Ruta ENABLE ALL TRIGGERS;
 
 /* INSTANCIAS PARA BARCOS */
     INSERT ALL 
-        INTO Barco (nombre, capacidad, nro_cabinas, nro_cubiertas, tonelaje, eslora,realiza_ruta) VALUES
-                    ('Buque ZENITH-ZT', 1442, 720, 10, 46811, 208,NULL)
-        INTO Barco (nombre, capacidad, nro_cabinas, nro_cubiertas, tonelaje, eslora,realiza_ruta) VALUES
-                    ('Buque MONARCH', 2766, 1193, 12, 73937, 268,NULL)
-        INTO Barco (nombre, capacidad, nro_cabinas, nro_cubiertas, tonelaje, eslora,realiza_ruta) VALUES
-                    ('Buque SOVEREIGN', 2733, 1162, 12, 73592, 268,NULL)
+        INTO Barco (id,nombre, capacidad, nro_cabinas, nro_cubiertas, tonelaje, eslora,realiza_ruta) VALUES
+                    (1,'Buque ZENITH-ZT', 1442, 720, 10, 46811, 208,NULL)
+        INTO Barco (id,nombre, capacidad, nro_cabinas, nro_cubiertas, tonelaje, eslora,realiza_ruta) VALUES
+                    (2,'Buque MONARCH', 2766, 1193, 12, 73937, 268,NULL)
+        INTO Barco (id,nombre, capacidad, nro_cabinas, nro_cubiertas, tonelaje, eslora,realiza_ruta) VALUES
+                    (3,'Buque SOVEREIGN', 2733, 1162, 12, 73592, 268,NULL)
     SELECT * from dual;
 
 /* INSTANCIAS PARA RUTAS */
     INSERT ALL
-        INTO Ruta (nombre, regimen, es_realizada) VALUES 
-                ('2 Noches - Andalucia', 'Todo Incluido',
+        INTO Ruta (id,nombre, regimen, es_realizada) VALUES 
+                (1,'2 Noches - Andalucia', 'Todo Incluido',
                 ( SELECT REF(oc) FROM Barco oc WHERE oc.nombre = 'Buque SOVEREIGN' ) )
-        INTO Ruta (nombre, regimen, es_realizada) VALUES 
-                ('2 Noches - Barcelona', 'Todo Incluido',
+        INTO Ruta (id,nombre, regimen, es_realizada) VALUES 
+                (2,'2 Noches - Barcelona', 'Todo Incluido',
                 ( SELECT REF(oc) FROM Barco oc WHERE oc.nombre = 'Buque ZENITH-ZT' ) )
-        INTO Ruta (nombre, regimen, es_realizada) VALUES 
-                ('4 Noches - Caribe Sur  (Puerto Limon)', 'Todo Incluido',
+        INTO Ruta (id,nombre, regimen, es_realizada) VALUES 
+                (3,'4 Noches - Caribe Sur  (Puerto Limon)', 'Todo Incluido',
                 ( SELECT REF(oc) FROM Barco oc WHERE oc.nombre = 'Buque MONARCH') )
     SELECT * from dual;
-
-    UPDATE Barco
-    SET realiza_ruta =  ( SELECT REF(oc) FROM Ruta oc WHERE oc.nombre = '2 Noches - Andalucia')
-    WHERE nombre = 'Buque ZENITH-ZT';
-
-    UPDATE Barco
-    SET realiza_ruta =  ( SELECT REF(oc) FROM Ruta oc WHERE oc.nombre = '2 Noches - Barcelona')
-    WHERE nombre = 'Buque MONARCH';
-
-    UPDATE Barco
-    SET realiza_ruta =  ( SELECT REF(oc) FROM Ruta oc WHERE oc.nombre = '4 Noches - Caribe Sur  (Puerto Limon)')
-    WHERE nombre = 'Buque SOVEREIGN';
 
 
 /* -------------------------------------------- CONSULTAS -------------------------------------------- */
